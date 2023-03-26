5966aee2b4fa28472949d840a8328fc669e23d37
Zonylaw <sunny_law@hotmail.com>
2023-03-26 18:15:49 +0100

    add delete function

--- todo/views.py
+++ todo/views.py
@@ -23,6 +23,7 @@ def add_item(request):
     context = {
         'form': form
     }
+    print(request)
     return render(request, "todo/add_item.html", context)
 
 
@@ -39,3 +40,17 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    # print(request)
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
